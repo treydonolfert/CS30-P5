d1f55d5d938d34a656bb7d46467bdd68857621f4
treydonolfert <treydonolfert1@gmail.com>
2024-02-15 15:18:52 -0600

    Completed github/vscode setup
    done

---
+++ 00 first project test/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  circle(mouseX, mouseY, 30);
+}
