e8e6ba2ace1e766395e68f394ac18580b64d5c99
treydonolfert <treydonolfert1@gmail.com>
2024-02-26 13:06:02 -0600

    challenge

--- 01 challenge/sketch.js
+++ 01 challenge/sketch.js
@@ -7,36 +7,54 @@
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 let state = 0;
-let x = 50;
+let x = 0;
 let y = 0;
+let z = 25;
+let speed = 10;
 function draw() {
   background(220);
-  square(x,y,50);
+  square(x,y,z);
   if (state === 0) {
-    x += 10;
+    x += speed;
     if (x === 350) {
       state = 1;
     }
   }
   if (state === 1) {
-    y += 10;
+    y += speed;
     if (y === 350) {
       state = 2;
     }
   }
   if (state === 2) {
-    x -= 10;
+    x -= speed;
     if (x === 0) {
       state = 3;
     }
   }
   if (state === 3) {
-    y -= 10;
+    y -= speed;
     if (y === 0) {
       state = 0;
     }
+  }
+}
+
+function keyPressed(){
+  if (keyCode === 65){
+    z = 5;
+  }
+  if (keyCode === 68){
+    z = 50;
+  }
+
+  if (keyCode === 83){
+    speed -= 5;
+  }
+  if (keyCode === 87){
+    speed = 50;
   }
 }
