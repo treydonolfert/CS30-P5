27fb161816ba1786ad03a3e8ac8c05ec3c097bab
treydonolfert <treydonolfert1@gmail.com>
2024-02-28 15:22:22 -0600

    Update sketch.js

--- 02 Drawing With Loops 1/sketch.js
+++ 02 Drawing With Loops 1/sketch.js
@@ -5,11 +5,41 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let xPos, yPos;
+let color = 0;
 
 function setup() {
   createCanvas(400, 300);
-  xPos = [width*0.05,width*0.95,width*0.05,width*0.95];
-  yPos = [height*0.05,height*0.05,height*0.95,height*0.95];
+  xPos = []; yPos = [];
+  outRing();
+  //xPos = [width*0.05,width*0.95,width*0.05,width*0.95];
+  //yPos = [height*0.05,height*0.05,height*0.95,height*0.95];
+}
+
+function initWithLoops() {
+  for (let x = 10; x < width; x += 20) {
+    xPos.push(x);
+    yPos.push(height/2);
+  }
+}
+
+function outRing() {
+  for (let x = 0; x <= width; x += 40) {
+    xPos.push(x);
+    yPos.push(0);
+  }
+  for (let x = 0; x <= height; x += 30) {
+    xPos.push(0);
+    yPos.push(x);
+  }
+  for (let x = 400; x >= 0; x -= 40) {
+    xPos.push(x);
+    yPos.push(300);
+  }
+  for (let x = 300; x >= 0; x -= 30) {
+    xPos.push(400);
+    yPos.push(x);
+  }
 }
 
 function draw() {
@@ -28,8 +58,27 @@ let i = 0;
 while (i < xPos.length) {
   let x = xPos[i];
   let y = yPos[i];
+  if (color === 0) {
+    fill(255,0,0);
+    color = 1;
+  }
+  else if (color === 1) {
+    fill(0,255,0);
+    color = 2;
+  }
+  else if (color === 2) {
+    fill (0,0,255);
+    color = 3;
+  }
+  else if (color === 3) {
+    fill(255,255,0);
+    color = 0;
+  }
+  print(color);
+  triangle(xPos[i-1],yPos[i-1],x,y,mouseX,mouseY);
+  fill(0,0,0);
   circle(x,y,20);
-  line(x,y,mouseX,mouseY);
+  //line(x,y,mouseX,mouseY);
   i++;
 }
 circle(mouseX,mouseY,20);
@@ -38,7 +87,6 @@ circle(mouseX,mouseY,20);
 
 
 function cornersAndMouse() {
-  fill(255);
   circle(width*0.05, height*0.05, 20);
   circle(width*0.95, height*0.05, 20);
   circle(width*0.05, height *0.95, 20);
