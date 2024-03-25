ab7e9e74b9e213a88a6bcc6e0a81baf227480ee7
treydonolfert <treydonolfert1@gmail.com>
2024-03-25 15:21:30 -0600

    generative art 3 demo

---
+++ 010c Generative Art 3/sketch.js
@@ -0,0 +1,32 @@
+// Generative Art - Repetition
+// Treydon Olfert
+// March 25, 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
+  stroke(0,80);
+}
+
+function randomElement(currentLen) {
+  push();
+  rotate(random(2*PI));
+  while(currentLen > 5) {
+    rotate(random(-PI/4,PI/4));
+    line(0,0,0,currentLen);
+    translate(0,currentLen);
+    currentLen *= 0.75;
+  }
+  pop();
+}
+
+function draw() {
+  translate(width/2,height/2);
+  for(let i = 0; i < 1e7; i++) {
+    randomElement(75);
+  }
+}
