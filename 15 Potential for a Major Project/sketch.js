076b46ff8a7c91cce91f51a3f2e2f2dc31a600df
treydonolfert <treydonolfert1@gmail.com>
2024-04-11 14:38:25 -0600

    potential project

---
+++ 15 Potential for a Major Project/sketch.js
@@ -0,0 +1,27 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+const g = 9.81;
+let scale;
+
+function findDisplacement(v0,a) {
+  return v0 * deltaTime + (1/2) * a * deltaTime ** 2;
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+class Ball {
+  constructor(vX, vY) {
+
+  }
+}
