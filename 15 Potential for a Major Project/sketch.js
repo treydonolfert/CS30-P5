8b36800d545914ceb773ee1fd69ab2a092268bfe
treydonolfert <treydonolfert1@gmail.com>
2024-04-11 18:34:54 -0600

    some sort of outline done

--- 15 Potential for a Major Project/sketch.js
+++ 15 Potential for a Major Project/sketch.js
@@ -5,12 +5,9 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const g = 9.81;
-let scale;
-
-function findDisplacement(v0,a) {
-  return v0 * deltaTime + (1/2) * a * deltaTime ** 2;
-}
+const g = -9.81;
+let scale = 10; //used for converting meters to pixels
+let balls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,10 +15,34 @@ function setup() {
 
 function draw() {
   background(220);
+  text("1 pixel = 0.1 meters", 0, 20);
+  for (i of balls) {
+    i.move();
+    i.display();
+  }
 }
 
 class Ball {
-  constructor(vX, vY) {
-
+  constructor(vX,vY) {
+    this.vX = vX;
+    this.vY = vY;
+    this.x = mouseX;
+    this.y = mouseY;
   }
+
+  move() {
+    this.vY += g * (deltaTime/1000);
+    this.x += scale * this.vX * (deltaTime/1000);
+    this.y -= scale * this.vY * (deltaTime/1000);
+  }
+
+  display() {
+    circle(this.x, this.y, 25);
+    line(this.x, this.y, this.x + 10 * this.vX, this.y);
+    line(this.x, this.y, this.x, this.y - 10 * this.vY);
+  }
+}
+
+function mousePressed() {
+  balls.push(new Ball(20,20));
 }
