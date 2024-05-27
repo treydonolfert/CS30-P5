cf1a47084fd315f9a2103cf01b94d39097d4b045
treydonolfert <treydonolfert1@gmail.com>
2024-05-27 15:19:23 -0600

    Worked on Kinematic Balls
    Clicking spawns a ball now and shows projectile motion as intended. Scale and g constant have been added as well.

--- 25 Physics Capstone/KinematicsBall.js
+++ 25 Physics Capstone/KinematicsBall.js
@@ -1,17 +1,18 @@
 class Ball {
-    constructor(x, y, vx, vy, ax, ay) {
+    constructor(x, y, vx, vy, ax, ay, scale) {
         this.pos = createVector(x, y);
-        this.vel = createVector(vx, vy);
-        this.acc = createVector(ax, ay);
+        this.vel = createVector(vx, -1 * vy);
+        this.acc = createVector(ax, -1 * ay);
+        this.scale = scale;
     }
 
     move() {
-        this.vel.add(acc);
-        this.pos.add(vel);
+        this.vel.add(this.acc.x * (deltaTime/1000), this.acc.y * (deltaTime/1000));
+        this.pos.add(this.vel.x * (deltaTime/1000) * scale, this.vel.y * (deltaTime/1000) * scale);
     }
 
-    display(){
-
+    display() {
+        circle(this.pos.x, this.pos.y, 250);
     }
     
 }
--- 25 Physics Capstone/sketch.js
+++ 25 Physics Capstone/sketch.js
@@ -2,6 +2,8 @@
 // Treydon Olfert
 // May 23, 2024
 
+let balls = [];
+let scale = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,6 +11,15 @@ function setup() {
 
 function draw() {
   background(220);
+  text("1 pixel = " + 1/scale + " meters", 0, 20);
+  for (i of balls) {
+    i.move();
+    i.display();
+  }
+}
+
+function mousePressed() {
+  balls.push(new Ball(mouseX, mouseY, 10, 20, 0, -9.8, scale));
 }
 
 /*Helpful Things I found:
